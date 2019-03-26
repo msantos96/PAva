e178077968872aefc501137423169eb5aa225fe7
msantos96 <marcosantoniosantos7@hotmail.com>
2019-03-26 21:56:10 +0000

    FunctionalCounter

---
+++ src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler/ProfilerTranslator.java
@@ -0,0 +1,64 @@
+package ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler;
+
+import javassist.*;
+import javassist.expr.*;
+
+public class ProfilerTranslator implements Translator {
+    private String mainClassName;
+    public ProfilerTranslator(String mainClassName) throws NotFoundException {
+        this.mainClassName = mainClassName;
+    }
+    
+    public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
+        CtClass mainClass = pool.get(mainClassName);
+        CtMethod main = mainClass.getDeclaredMethod("main");
+        CtField ctField = CtField.make("public static ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler.RWCounter __rwCounters = new ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler.RWCounter();", mainClass);
+        mainClass.addField(ctField);
+        main.insertAfter(" { __rwCounters.printProfiles(); } ");
+    }
+
+    public void onLoad(ClassPool pool, String className) throws NotFoundException, CannotCompileException {
+        CtClass ctClass = pool.get(className);
+        try {
+            if(className.equals("ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler.RWCounter")) return;
+            if(!ctClass.isInterface())  profile(ctClass);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+        2 reads no msm metodo é suposto ??
+        read write no objecto a do objecto b soma no objecto a ou b
+    */
+    private void profile(CtClass ctClass) throws ClassNotFoundException, NotFoundException, CannotCompileException {
+        CtField ctField = CtField.make("public static int[] __rwCounter = new int[2];", ctClass);
+        ctClass.addField(ctField);
+
+        for(CtConstructor ctConstructor : ctClass.getDeclaredConstructors()) {
+            int[] __rwCounter = new int[2];
+            ctConstructor.instrument(new ExprEditor() {
+                public void edit(FieldAccess fa) throws CannotCompileException {
+                    if (fa.isStatic()) return;
+                    if (fa.isReader())  __rwCounter[0]++;
+                    if (fa.isWriter())  __rwCounter[1]++;
+                }
+            });
+            ctConstructor.insertAfter(" { __rwCounter[0] += " + __rwCounter[0] + "; __rwCounter[1] += " + __rwCounter[1] + "; } ");
+
+            ctConstructor.insertAfter(" { " + mainClassName + ".__rwCounters.putIfAbsent(\"" + ctClass.getName() + "\", __rwCounter); } ");
+        }
+
+        for(CtMethod ctMethod : ctClass.getDeclaredMethods()) {
+            int[] __rwCounter = new int[2];
+            ctMethod.instrument(new ExprEditor() {
+                public void edit(FieldAccess fa) throws CannotCompileException {
+                    if (fa.isStatic()) return;
+                    if (fa.isReader())  __rwCounter[0]++;
+                    if (fa.isWriter())  __rwCounter[1]++;
+                }
+            });
+            ctMethod.insertAfter(" { __rwCounter[0] += " + __rwCounter[0] + "; __rwCounter[1] += " + __rwCounter[1] + "; } ");
+        }
+    }
+}
---
+++ src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler/RWCounter.java
@@ -0,0 +1,20 @@
+package ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler;
+
+import java.util.HashMap;
+
+public class RWCounter {
+    private HashMap<String, int[]> __rwCounters = new HashMap<String, int[]>();
+    public void putIfAbsent(String key, int[] value) {
+        __rwCounters.putIfAbsent(key, value);
+    }
+    public void printProfiles() {
+        int[] counter = new int[2];
+        for(int[] c : __rwCounters.values()) {
+            counter[0] += c[0];
+            counter[1] += c[1];
+        }
+        System.out.println("\nTotal reads: " + counter[0] + " Total writes: " + counter[1]);
+        for(String key : __rwCounters.keySet())
+            System.out.println("class " + key + " -> reads: " + __rwCounters.get(key)[0] + " write: " + __rwCounters.get(key)[1]);
+    }
+}
