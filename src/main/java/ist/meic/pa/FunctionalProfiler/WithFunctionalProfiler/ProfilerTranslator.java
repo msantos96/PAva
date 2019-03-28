2bd1ebf5a1ee904249566cf5f212854ac5036963
msantos96 <marcosantoniosantos7@hotmail.com>
2019-03-28 01:36:15 +0000

    Skip

--- src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler/ProfilerTranslator.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler/ProfilerTranslator.java
@@ -3,6 +3,8 @@ package ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler;
 import javassist.*;
 import javassist.expr.*;
 import java.util.HashMap;
+import java.util.Arrays;
+import ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler.Skip;
 
 public class ProfilerTranslator implements Translator {
     private String mainClassName;
@@ -24,10 +26,7 @@ public class ProfilerTranslator implements Translator {
     public void onLoad(ClassPool pool, String className) throws NotFoundException, CannotCompileException {
         CtClass ctClass = pool.get(className);
         try {
-            if(className.startsWith("java")) return;
-            if(className.equals(mainClassName)) return;
-            if(className.equals("ist.meic.pa.FunctionalProfiler.WithFunctionalProfiler.RWCounter")) return;
-
+            if(ctClass.hasAnnotation​(Skip.class)) return;
             profile(pool, className, ctClass.getDeclaredConstructors());
             profile(pool, className, ctClass.getDeclaredMethods());
         } catch (ClassNotFoundException e) {
@@ -40,13 +39,15 @@ public class ProfilerTranslator implements Translator {
             HashMap<String, int[]> __rwCounters = new HashMap<String, int[]>();
             ctBehavior.instrument(new ExprEditor() {
                 public void edit(FieldAccess fa) throws CannotCompileException {
+                    if(fa.isStatic()) return;
                     String cName = fa.getClassName();
+                    
                     int[] __rwCounter = __rwCounters.getOrDefault(cName, new int[2]);
                     __rwCounters.putIfAbsent(cName, __rwCounter);
-                    if(ctBehavior instanceof CtConstructor && fa.getClassName().equals(className)) return;
-                    if (fa.isStatic()) return;
-                    if (fa.isReader())  __rwCounter[0]++;
-                    if (fa.isWriter())  __rwCounter[1]++;
+
+                    if(ctBehavior instanceof CtConstructor && cName.equals(className)) return;
+                    if(fa.isReader())  __rwCounter[0]++;
+                    if(fa.isWriter())  __rwCounter[1]++;
                 }
             });
             
@@ -54,10 +55,10 @@ public class ProfilerTranslator implements Translator {
                 if(!java.util.Arrays.stream(pool.get(key).getDeclaredFields()).map(f -> f.getName()).anyMatch(fName -> fName.equals("__rwCounter")))
                     pool.get(key).addField(CtField.make("public static int[] __rwCounter = new int[2];", pool.get(key)));
                 
-                ctBehavior.insertAfter(" { " + key + ".__rwCounter[0] += " + __rwCounters.get(key)[0] + "; " + key + ".__rwCounter[1] += " + __rwCounters.get(key)[1] + "; } ");
+                ctBehavior.insertBefore(" { " + key + ".__rwCounter[0] += " + __rwCounters.get(key)[0] + "; " + key + ".__rwCounter[1] += " + __rwCounters.get(key)[1] + "; } ");
 
                 if(ctBehavior instanceof CtConstructor)
-                    ctBehavior.insertAfter(" { " + mainClassName + ".__rwCounters.putIfAbsent(\"" + className + "\", __rwCounter); } ");
+                    ctBehavior.insertBefore(" { " + mainClassName + ".__rwCounters.putIfAbsent(\"" + className + "\", __rwCounter); } ");
             }
         }
     }
