484db0b6453ad0c8108931c3d5d1b020aac3b5fc
msantos96 <marcosantoniosantos7@hotmail.com>
2019-05-15 09:54:07 +0100

    test

--- 2/JOS.jl
+++ 2/JOS.jl
@@ -17,20 +17,12 @@ make_class(name, superclasses, slots) = (
     Class(name, superclasses, slots)
 )
 
-#C1 = make_class(:C1, [], [:a])
-#C2 = make_class(:C2, [], [:b, :c])
-#C3 = make_class(:C3, [C1, C2], [:d])
-
-macro defclass(expr)
+macro defclass(expr) #method?????
     name = expr.args[1]
     superclasses = expr.args[2].args
     slots = expr.args[3:end]
     :($(esc(name)) = make_class($(Base.Meta.quot(name)), $superclasses, $slots))
 end
-
-@defclass (C1, [], a) #space????
-@defclass (C2, [], b, c)
-@defclass (C3, [C1, C2], d)
 
 #when typeof is used in a instance???? should return class????
 mutable struct Instance
@@ -49,9 +41,6 @@ end
 
 make_instance(class, slot_val...) = Instance(class, slot_val...)
 
-c3i1 = make_instance(C3, :a=>1, :b=>2, :c=>3, :d=>4)
-c3i2 = make_instance(C3, :b=>2)
-
 #change output when > c3i1 to show class ????
 
 #should redefine instance and class get slot so users cant change it???? if so should we remove the "introspectable" fields????
@@ -62,8 +51,8 @@ function get_slot(x::Instance, field::Symbol)
     catch error
         if isa(error, KeyError)
             #should not show stack trace??
-            throw(string("Slot ", string(field),
-                any(x -> x == field, getfield(x, :class).slots) ? " is unbound" : " is missing"))
+            throw(error(string("ERROR: Slot " , string(field),
+                any(x -> x == field, getfield(x, :class).slots) ?  " is unbound" : " is missing"))
         else
             throw(error)
         end
@@ -74,23 +63,6 @@ set_slot!(x::Instance, field::Symbol, value) = getfield(x, :slot_val)[field] = v
 
 Base.getproperty(x::Instance, field::Symbol) = get_slot(x, field)
 Base.setproperty!(x::Instance, field::Symbol, value) = set_slot!(x, field, value)
-
-get_slot(c3i2, :b)
-#2
-set_slot!(c3i2, :b, 3)
-#3
-println([get_slot(c3i1, s) for s in [:a, :b, :c]])
-#[1, 2, 3]
-c3i1.a
-#1
-c3i1.e
-#ERROR: Slot e is missing
-c3i2.a
-#ERROR: Slot a is unbound
-c3i2.a = 5
-#5
-c3i2.a
-#5
 
 mutable struct Generic
     name
@@ -142,21 +114,12 @@ macro defmethod(expr)
     )
 end
 
-@defgeneric foo(c)
-
-#error msg if called before foo
-@defmethod foo(c::C1) = 1
-
-@defmethod foo(c::C2) = c.b
-
-@defmethod foo(c::C2, c1::C1) = c.b + 1
-
 function getEffectiveMethod(methods, args...)
     #also verify when type does not exist - extends?????
     #flavors approach???
     for m in methods
         if length(args) == length(m.parameters) &&
-                tuple([p.args[2] for p in m.parameters]...) == tuple([getfield(a, :class).name for a in args]...)
+                tuple([p.args[2] for p in m.parameters]...) == tuple([getfield(a, :class).name for a in args]...) #applicable!!!!!!
             return m
         end
     end
@@ -164,27 +127,3 @@ function getEffectiveMethod(methods, args...)
 end
 
 (f::Generic)(args...) = getEffectiveMethod(f.methods, args...)(args...)
-
-foo(make_instance(C1))
-#1
-foo(make_instance(C2, :b=>42))
-#42
-
-@defgeneric bar(x, y)
-@defmethod bar(x::C1, y::C2) = x.a + y.b
-@defmethod bar(x::C1, y::C3) = x.a - y.b
-@defmethod bar(x::C3, y::C3) = x.a * y.b
-
-c1i1 = make_instance(C1, :a=>1)
-c2i1 = make_instance(C2, :b=>3)
-c3i1 = make_instance(C3, :a=>1, :b=>2)
-c3i2 = make_instance(C3, :b=>3, :a=>5)
-
-bar(c1i1, c2i1)
-#4
-bar(c2i1, c1i1)
-#ERROR: No applicable method
-bar(c1i1, c3i1)
-#-1
-bar(c3i1, c3i2)
-#3
